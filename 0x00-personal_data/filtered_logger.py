98f5850180dbc1d23640a5a331a1cf44c7c25eec
Uchenna Oko <uche4newton1981@gmail.com>
2024-04-12 01:42:36 +0100

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Regex-ing"""
 
+import logging
 import re
 from typing import List
 
@@ -16,3 +17,23 @@ def filter_datum(fields: List[str], redaction: str,
                          "{}={}{}".format(field, redaction, separator),
                          message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """This is init method"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+         """ This filters value in incomming log records """
+         rec.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
