ec51348abe29f3f56d9f4e504a245329d3148c08
Uchenna Oko <uche4newton1981@gmail.com>
2024-04-12 01:52:17 +0100

    Log formatter-error fixed

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,7 +6,7 @@ import re
 from typing import List
 
 
-# PII_FIELDS = ("name", "email", "password", "phone", "ssn")
+PII_FIELDS = ("name", "email", "password", "phone", "ssn")
 
 
 def filter_datum(fields: List[str], redaction: str,
