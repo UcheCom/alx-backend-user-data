37e045365f19e478f9ad29d69c60c6d1c5597083
Uchenna Oko <uche4newton1981@gmail.com>
2024-04-17 23:27:34 +0100

    Updating api

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Module to manage the API authentication"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+   """Class manages authentication"""
+
+
+   def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+       """Method defines the required authentication"""
+       return False
+
+
+   def authorization_header(self, request=None) -> str:
+        """ Method used to create the authentication header
+        Return:
+          - String
+        """
+        return None
+
+
+   def current_user(self, request=None) -> TypeVar('User'):
+        """Method defines the current user"""
+        return None
