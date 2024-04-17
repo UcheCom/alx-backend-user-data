8ff324da27020e60eac38ce1d7593b152c8539be
Uchenna Oko <uche4newton1981@gmail.com>
2024-04-17 17:14:51 +0100

    Updating api

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python3
 """
-Route module for the API
+This is Route module for the API
 """
+import os
 from os import getenv
-# from api.v1.views import app_views
+from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-# import os
 
 
 app = Flask(__name__)
-# app.register_blueprint(app_views)
+app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 # auth = None
+
+
+@app.errorhandler(401)
+def unauthorized(e) -> str:
+    """Unauthorized handler"""
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 if __name__ == "__main__":
