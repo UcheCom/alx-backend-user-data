aad488e45263ce905e50c5deb92dd11ae59b9f40
Uchenna Oko <uche4newton1981@gmail.com>
2024-04-17 23:35:45 +0100

    Updating api

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,22 +5,19 @@ from typing import List, TypeVar
 
 
 class Auth:
-   """Class manages authentication"""
+    """Class manages authentication"""
 
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Method defines the required authentication"""
+        return False
 
-   def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-       """Method defines the required authentication"""
-       return False
-
-
-   def authorization_header(self, request=None) -> str:
+    def authorization_header(self, request=None) -> str:
         """ Method used to create the authentication header
         Return:
           - String
         """
         return None
 
-
-   def current_user(self, request=None) -> TypeVar('User'):
+    def current_user(self, request=None) -> TypeVar('User'):
         """Method defines the current user"""
         return None
