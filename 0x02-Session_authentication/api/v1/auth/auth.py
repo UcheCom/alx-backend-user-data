c60d891e755f646dd02fedb60ae5d547940c6c4a
Uchenna Oko <uche4newton1981@gmail.com>
2024-04-20 20:47:44 +0100

    Adding api package

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""Module to manage the API authentication"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Class manages authentication"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Method defines the required authentication"""
+        if path is None:
+            return True
+
+        elif excluded_paths is None or excluded_paths == []:
+            return True
+
+        elif path in excluded_paths:
+            return False
+        else:
+            for i in excluded_paths:
+                if i.startswith(path):
+                    return False
+                if path.startswith(i):
+                    return False
+                if i[-1] == "*":
+                    if path.startswith(i[:-1]):
+                        return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ Method used to create the authentication header
+        Return:
+          - String
+        """
+        if request is None:
+            return None
+        header = request.headers.get('Authorization')
+        if header is None:
+            return None
+        return header
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Method defines the current user"""
+        return None
---
+++ 0x02-Session_authentication/api/v1/views/index.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+""" This module handles index views
+"""
+from flask import jsonify, abort
+from api.v1.views import app_views
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status() -> str:
+    """This GETs /api/v1/status
+    Return: the status of the API
+    """
+    return jsonify({"status": 'OK'})
+
+
+@app_views.route('/stats/', strict_slashes=False)
+def stats() -> str:
+    """This GETs /api/v1/stats
+    Return: the number of objects
+    """
+    from models.user import User
+    stats = {}
+    stats['users'] = User.count()
+    return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """This GETs /api/v1/unathorized
+    Return: an error message
+    """
+    abort(401, description='Unauthorized')
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """GET /api/v1/forbidden"""
+    abort(403, description='forbidden')
