53f1b9a5fa153e86b55994d7cd5b35a81da032ef
Uchenna Oko <uche4newton1981@gmail.com>
2024-04-24 11:27:53 +0100

    Updating api package

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -4,7 +4,7 @@
 import os
 from models.user import User
 from api.v1.views import app_views
-from flask import jsonify, request
+from flask import jsonify, request, abort
 from api.v1.auth.session_auth import SessionAuth
 
 
