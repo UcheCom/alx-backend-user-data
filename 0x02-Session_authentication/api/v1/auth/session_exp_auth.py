f99b117541f4a28fa0ec7223fb2de0c547533fe8
Uchenna Oko <uche4newton1981@gmail.com>
2024-04-24 14:55:37 +0100

    Updating api package

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -19,30 +19,30 @@ class SessionExpAuth(SessionAuth):
 
     def create_session(self, user_id: str = None) -> str:
         """This creates a session_id based on user_id"""
-        sess_id = super().create_session(user_id)
-        if not sess_id:
+        session_id = super().create_session(user_id)
+        if not session_id:
             return None
         sess_dict = {}
         sess_dict['user_id'] = user_id
         sess_dict['created_at'] = datetime.now()
-        self.user_id_by_session_id[sess_id] = sess_dict
+        self.user_id_by_session_id[session_id] = sess_dict
 
-        return sess_id
+        return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """ Thsi returns the user_id based on the session_id"""
-        sess_dict = self.user_id_by_session_id[session_id]
         if session_id is None:
             return None
-        if session_id not in self.user_id_by_session_id:
+        sess_dict = self.user_id_by_session_id[session_id]
+        if sess_dict is None:
             return None
         if self.session_duration <= 0:
-            return sess_dict['user_id']
-        if "craeted_at" not in sess_dict:
+            return sess_dict["user_id"]
+        if "craeted_at" not in sess_dict.keys():
             return None
         current_time = datetime.now()
         time_span = timedelta(seconds=self.session_duration)
         exp_time = sess_dict['created_at'] + time_span
         if exp_time < current_time:
             return None
-        return self.sess_dict["user_id"]
+        return self.sess_dict["user_id"]  # self.sess_dict.get('user_id')
