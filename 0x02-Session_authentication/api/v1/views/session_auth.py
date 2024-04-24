a7906b18547678c21ef40f97c65024ee57b41f21
Uchenna Oko <uche4newton1981@gmail.com>
2024-04-24 10:51:18 +0100

    Updating api package

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -2,4 +2,47 @@
 """ This is view to handle all routes for the Session authentication
 """
 import os
+from models.user import User
+from api.v1.views import app_views
 
+
+@app_views.route('/auth_session/login', methods=['POST'],
+                 strict_slashes=False)
+def login() -> str:
+    """POST /api/v1/auth_session/login
+    Return: a JSON
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not email:
+        return jsonify({"error": "email missing"}), 400
+    if not password:
+        return jsonify({"error": "password missing"}), 400
+
+    users = User.search({"email": email})
+    if not users:
+        return jsonify({"error": "no user found for this email"}), 404
+    for user in users:
+        if user.is_valid_password(password):
+            from api.v1.app import auth
+            sess_id = auth.create_session(user.id)
+            resp = jsonify(user.to_json())
+            sess_name = os.getenv('SESSION_NAME')
+            resp.set_cookie(sess_name, sess_id)
+            return resp
+    return jsonify({"error": "wrong password"}), 401
+
+
+@app_views.route("/auth_session/logout",
+                 methods=["DELETE"], strict_slashes=False)
+def logout() -> str:
+    """ DELETE api/v1/auth_session/logout
+    Return: a JSON
+    """
+    from api.v1.app import auth
+    if auth.destroy_session(request):
+        return jsonify({}), 200
+    abort(404)pp import auth
+    if auth.destroy_session(request):
+        return jsonify({}), 200
+    abort(404)
