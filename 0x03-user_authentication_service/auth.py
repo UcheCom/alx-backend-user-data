c8032314e0668a70e067cca3ec658b766f6d0373
Uchenna Oko <uche4newton1981@gmail.com>
2024-04-26 15:19:15 +0100

    Adding auth module

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -11,7 +11,7 @@ from sqlalchemy.orm.exc import NoResultFound
 def _hash_password(password: str) -> str:
     """ This returns a salted hash an input password
     """
-    return bcrypt.hashpw(password.encode('utf-8'), becrypt.gensalt())
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def _generate_uuid() -> str:
