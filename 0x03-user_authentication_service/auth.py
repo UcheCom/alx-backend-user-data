1e604b21e06f7afc9b0e965359852d8c7031dc93
Uchenna Oko <uche4newton1981@gmail.com>
2024-04-26 15:46:26 +0100

    Updating auth module

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -91,3 +91,13 @@ class Auth:
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """This updates the password using reset token"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError()
+
+        hashed = _hashed_password(password)
+        self._db.update_user(user.id, hashed_password=hashed, reset_token=None)
