691f27ade9e6749655458821884271fdc6dfe4e9
Uchenna Oko <uche4newton1981@gmail.com>
2024-04-27 22:12:05 +0100

    Updating app module

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """Basic flask app
 """
 
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 AUTH = Auth()
@@ -42,6 +42,53 @@ def login() -> str:
     resp = jsonify({"email": email, "message": "logged in"})
     resp.set_cookie("session_id", session_id)
     return resp
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """This Deletes a login info/session or logout user"""
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect('/')
+
+
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile() -> str:
+    """THis Gets the user profile"""
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    return jsonify({"email": user.email}), 200
+
+
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token() -> str:
+    """ This generates a token
+    """
+    email = request.form.get('email')
+    try:
+        reset_token = AUTH.get_reset_password_token('email')
+        return jsonify({"email": email, "reset_token": reset_token})
+
+    except Exception:
+        abort(403)
+
+
+@app.route('/reset_password', methods=['PUT'])
+def update_password() -> str:
+    """This updates the user's password"""
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"})
+    except ValueError:
+        abort(403)
 
 
 if __name__ == "__main__":
