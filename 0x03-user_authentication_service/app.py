0ca966585a5c46d49d683ee85ac640e0953ac70e
Uchenna Oko <uche4newton1981@gmail.com>
2024-04-27 19:19:37 +0100

    Updating app module

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """Basic flask app
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from auth import Auth
 
 AUTH = Auth()
@@ -12,8 +12,22 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index() -> str:
-    """This index route"""
+    """This index route
+    Return: The home page
+    """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """This posts/registers new users"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
